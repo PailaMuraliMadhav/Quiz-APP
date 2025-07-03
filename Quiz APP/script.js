20af6bf157028fd8e6bf110fd51f2b5871a73e84
Paila Murali Madhav <muralipaila1313@gmail.com>
2025-07-03 11:44:28 +0530

    js

---
+++ Quiz APP/script.js
@@ -0,0 +1,22 @@
+// startScreen
+
+const startScreen = document.getElementById("start-screen");
+const startButton = document.getElementById("start-button");
+// quizscreen
+const quizScreen = document.getElementById("quiz-screen");
+const questiontext = document.getElementById("question-text");
+const currentQuestionIndex = document.getElementById("current-question");
+const totalQuestions = document.getElementById("total-questions");
+const score = document.getElementById("score");
+// answer container
+const answersContainer = document.getElementById("answers-container");
+// progress bar
+const progressBar = document.getElementById("progress-bar");
+const progress = document.getElementById("progress");
+// results screen
+const resultsScreen = document.getElementById("results-screen");
+const finalScore = document.getElementById("final-score");
+const maxScore = document.getElementById("max-score");
+const resultmessage = document.getElementById("result-message");
+// restart 
+const restartButton = document.getElementById("restart-button");
